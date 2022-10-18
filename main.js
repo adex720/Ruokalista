33349d4b61d52866f0cd2f8e7b47f3a2f18b35c3
adex720 <25402611+adex720@users.noreply.github.com>
2022-10-18 17:45:09 +0300

    Change names to English

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-function alustaNapit() {
+function initButtons() {
     document.getElementById('edellinen').addEventListener('mouseup', () => { window.location.href = getNewPage(-1); }, false)
     document.getElementById('seuraava').addEventListener('mouseup', () => { window.location.href = getNewPage(+1); }, false)
 }
 
+/**
+ * Refreshes the current page to a new date.
+ * Day difference is determined with url parameters
+ */
 function getNewPage(delta) {
     var current = window.location.href;
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
 
     if (!queryString.includes('?')) {
+        // No url parameters exist
         return current + '?paiva=' + delta;
     }
     if (urlParams.has('paiva')) {
+        // 'paiva' url parameter exists
         var paiva = parseInt(urlParams.get('paiva')) + delta;
 
         if (paiva >= 7) paiva = paiva % 7;
@@ -20,19 +26,20 @@ function getNewPage(delta) {
         urlParams.set('paiva', paiva);
         return window.location.pathname + '?' + urlParams.toString();
     }
+    // Other url parameters exist
     return current + '&paiva=' + delta;
 }
 
+/**
+ * Requests a JSON file from an url and runs callback with response.
+ */
 function getJSON(url, callback) {
-
     let xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
 
     xhr.onload = () => {
-
         let status = xhr.status;
-
         if (status == 200) {
             callback(null, xhr.response);
         } else {
@@ -43,122 +50,148 @@ function getJSON(url, callback) {
     xhr.send();
 }
 
-function teeRuokalista() {
+/**
+ * Fetches menu of the week from www.sodexo.fi as a json.
+ * Updates content of page to show menu of the day.
+ */
+function createMenu() {
     const response = getJSON('https://www.sodexo.fi/ruokalistat/output/weekly_json/84', (err, data) => {
 
         if (err != null) {
             console.error(err);
         } else {
-            var tanaan = data.mealdates[getDayId()];
+            var menu = data.mealdates[getDayId()];
 
-            if (tanaan == undefined) {
-                eiKoulua();
+            if (menu == undefined) {
+                // The restaurant isn't open
+                noSchool();
                 return;
             }
-            var ruuat = tanaan.courses;
+            var courses = menu.courses;
 
-            var sekaruoka;
-            var kasvisruoka;
+            var meatCourse;
+            var vegetarianCourse;
 
-            if (ruuat[2] != undefined) {
-                sekaruoka = ruuat[1].title_fi;
-                kasvisruoka = ruuat[2].title_fi;
+            if (courses[2] != undefined) {
+                // 2 courses
+                meatCourse = courses[1].title_fi;
+                vegetarianCourse = courses[2].title_fi;
             } else {
-                sekaruoka = undefined;
-                kasvisruoka = ruuat[1].title_fi;
+                // Only vegetarian course
+                meatCourse = undefined;
+                vegetarianCourse = courses[1].title_fi;
             }
 
-            paivita(sekaruoka, kasvisruoka,);
+            update(meatCourse, vegetarianCourse);
         }
 
     });
 }
 
+/**
+ * Returns Date object with correct day difference.
+ */
 function getDate() {
     var date = new Date();
-    var newDay = date.getDate() + paivaEro;
+    var newDay = date.getDate() + dayDiference;
     date.setDate(newDay);
     return date;
 }
 
+/**
+ * Returs id of the current week day.
+ * Monday=0, Tuesday=1, ... Saturday=5, Sunday=-1
+ */
 function getDayId() {
     return getDate().getDay() - 1;
 }
 
-function paivanNimi() {
+/**
+ * Returns the name of the current day and date on Finnish
+ */
+function nameOfDay() {
     var date = getDate();
 
-    var paiva = date.getDate();
-    var kuukausi = date.getMonth() + 1;
-    var viikonpaivaId = date.getDay();
+    var dateOfMonth = date.getDate();
+    var month = date.getMonth() + 1;
+    var dayOfWeekId = date.getDay();
 
-    var viikonpaiva;
-    switch (viikonpaivaId) {
-        case 0: viikonpaiva = 'Su'; break;
-        case 1: viikonpaiva = 'Ma'; break;
-        case 2: viikonpaiva = 'Ti'; break;
-        case 3: viikonpaiva = 'Ke'; break;
-        case 4: viikonpaiva = 'To'; break;
-        case 5: viikonpaiva = 'Pe'; break;
-        case 6: viikonpaiva = 'La';
+    var dayOfWeek;
+    switch (dayOfWeekId) {
+        case 0: dayOfWeek = 'Su'; break;
+        case 1: dayOfWeek = 'Ma'; break;
+        case 2: dayOfWeek = 'Ti'; break;
+        case 3: dayOfWeek = 'Ke'; break;
+        case 4: dayOfWeek = 'To'; break;
+        case 5: dayOfWeek = 'Pe'; break;
+        case 6: dayOfWeek = 'La';
     }
 
-    return viikonpaiva + ' ' + paiva + '.' + kuukausi + '.';
+    return dayOfWeek + ' ' + dateOfMonth + '.' + month + '.';
 }
 
-function paivita(sekaruoka, kasvisruoka) {
-    document.getElementById('otsikko').textContent = paivanNimi() + ' ruokalista:';
+/**
+ * Updates html elements to contain course names.
+ */
+function update(meatCourse, vegetarianCourse) {
+    document.getElementById('otsikko').textContent = nameOfDay() + ' ruokalista:';
+    document.getElementById('ruoka_ka').textContent = vegetarianCourse;
 
-    if (sekaruoka != undefined) {
-        document.getElementById('ruoka_sr').textContent = sekaruoka;
-        document.getElementById('ruoka_ka').textContent = kasvisruoka;
-
+    if (meatCourse != undefined) {
+        document.getElementById('ruoka_sr').textContent = meatCourse;
         return;
     }
 
-    document.getElementById('seka').style.display = 'none';
-    document.getElementById('ruoka_ka').textContent = kasvisruoka;
+    document.getElementById('seka').style.display = 'none'; // Hide meal course elements if only vegetarian course is server.
 }
 
-function eiKoulua() {
-    document.getElementById('otsikko').textContent = paivanNimi() + ' ruokalista:';
+/**
+ * Removes course elements and adds a new header informing no school.
+ */
+function noSchool() {
+    // Updating date
+    document.getElementById('otsikko').textContent = nameOfDay() + ' ruokalista:';
+
+    // Creating element informing no school
     const otsikko = document.createElement('h1');
     otsikko.textContent = 'Tänään ei ole koulua ):';
     otsikko.className = 'info';
     document.getElementById('otsikko').parentElement.appendChild(otsikko);
 
+    // Removing course elements
     document.getElementById('seka').style.display = 'none';
     document.getElementById('kasvis').style.display = 'none';
 }
 
-function laskePaivaEro() {
+function calculateDayDifference() {
+    // Getting 'paiva' url param
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    var paiva = urlParams.get('paiva');
-    if (paiva == undefined) return 0;
-    paiva = parseInt(paiva);
+    var day = urlParams.get('paiva');
+    if (day == undefined) return 0;
+    day = parseInt(day);
 
-    var ero;
-    if (paiva > -7 && paiva < 7) {
-        if (paiva >= 0) ero = paiva;
-        else ero = 7 + paiva;
-    } else {
-        paiva = paiva % 7;
-        if (paiva >= 0) ero = paiva;
-        else ero = 7 - paiva;
+    // Parsing day difference (ex. 11->4, -1->6)
+    var difference;
+    if (day <= -7 || day >= 7) {
+        day = day % 7;
     }
 
+    if (day >= 0) difference = day;
+    else difference = 7 + day;
+
+    // Moving selected date to the same week as the current day is while remaining day of the week
     var tanaanPaivaId = new Date().getDay() - 1;
     if (tanaanPaivaId == -1) tanaanPaivaId = 6;
 
-    var uusiPaivaId = tanaanPaivaId + ero;
+    var uusiPaivaId = tanaanPaivaId + difference;
     if (uusiPaivaId >= 7) uusiPaivaId -= 7;
 
-    if (uusiPaivaId < tanaanPaivaId) ero -= 7;
-    return ero;
+    if (uusiPaivaId < tanaanPaivaId) difference -= 7;
+    return difference;
 }
 
-var paivaEro = laskePaivaEro();
+var dayDiference = calculateDayDifference();
 
-alustaNapit();
-teeRuokalista();
+initButtons();
+createMenu();
